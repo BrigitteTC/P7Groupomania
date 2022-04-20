4740219a75afcdcea116a0db6cefac9f4793720f
Brigitte Tricot-Censier <brigittetc.pub@hotmail.fr>
2022-04-20 19:01:10 +0200

    init routes/user.js.

---
+++ backend/routes/user.js
@@ -0,0 +1,23 @@
+/*----------------------------------------------------
+user.js
+router pour les utilisateurs.
+Auteur: BTC/
+Date de création: 20/04/2022
+----------------------------------------------------------
+*/
+//declaration express et son router
+const express = require("express");
+const router = express.Router();
+
+// controleur pour associer les fonctions aux différentes routes
+const userCtrl = require("../controllers/user");
+
+//routes POST pour signup et login envoyé par le frontend
+router.post("/signup", userCtrl.signup);
+router.post("/login", userCtrl.login);
+
+//Suppression d'un utilisateur  DELETE
+router.delete("/:id", auth, userCtrl.deleteUser);
+
+//export du router
+module.exports = router;
