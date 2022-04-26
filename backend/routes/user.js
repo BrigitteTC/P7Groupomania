e93209e18955bdc3e1fb7c9148481482acb0ee29
Brigitte Tricot-Censier <brigittetc.pub@hotmail.fr>
2022-04-26 17:56:25 +0200

    routes/user.js: correction route delete et get: ajout /login.

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -11,21 +11,21 @@ Date de création: 20/04/2022
 const express = require("express");
 const router = express.Router();
 
-//Authentification pour vérifier le user
-const auth = require("../middleware/auth");
-
 // controleur pour associer les fonctions aux différentes routes
 const userCtrl = require("../controllers/user");
+
+//Authentification pour vérifier le user
+const auth = require("../middleware/auth");
 
 //routes POST pour signup et login envoyé par le frontend
 router.post("/signup", userCtrl.signup);
 router.post("/login", userCtrl.login);
 
 //Suppression d'un utilisateur  DELETE
-router.delete("/:id", auth, userCtrl.deleteUser);
+router.delete("/login/:id", auth, userCtrl.deleteUser);
 
 //GET
-router.get("/", userCtrl.getAllUsers);
+router.get("/login", userCtrl.getAllUsers);
 
 //export du router
 module.exports = router;
